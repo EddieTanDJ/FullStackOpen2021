9780b603a61aa075913c843dd9659ba250d28439
EddieTanDJ <2002226@sit.singaporetech.edu.sg>
2021-12-19 21:39:48 +0800

    Added Practice and CourseInfo

---
+++ part_1/WORKFLOW.md
@@ -0,0 +1,3 @@
+1. part1
+a. practice
+b. courseinfo
---
+++ part_1/pratice/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  // App is the root of the project
+  document.getElementById('root')
+);
