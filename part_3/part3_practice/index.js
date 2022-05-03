d7c5fe8296348e9a24e42ee679a07a3468befc98
EddieTanDJ <2002226@sit.singaporetech.edu.sg>
2022-05-03 23:36:15 +0800

    Ex3.1-3.8

---
+++ part_3/part3_practice/index.js
@@ -0,0 +1,95 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let notes = [
+    {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true
+    },
+    {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false
+    },
+    {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true
+    }
+]
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => {
+        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return note.id === id
+    })
+    console.log(note)
+    if(note) {
+        response.json(note)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    console.log(request.params.id)
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    /**
+     * notes.map(n => n.id) creates a new array that contains all the ids of the notes. Math.max returns the maximum value of the numbers that are passed to it. 
+     * However, notes.map(n => n.id) is an array so it can't directly be given as a parameter to Math.max. 
+     * The array can be transformed into individual numbers by using the "three dot" spread syntax ...
+     */
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+    return maxId + 1
+}
+
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    console.log(body)
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+    console.log(note)
+    notes = notes.concat(note)
+    console.log(notes)
+    response.json(note)
+})
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
---
+++ part_3/phonebook/index.js
@@ -0,0 +1,116 @@
+const { response, request } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+app.use(morgan(':method :url :status :res[body-length] - :response-time ms :body'))
+// app.use(morgan('tiny'))
+app.use(express.json())
+
+let phonebook = 
+[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+// Create a new token for logging using morgan
+//JSON.stringify() method converts a JavaScript object or value to a JSON string.
+morgan.token('body', (req, res) => 
+    request.method === 'POST'
+    ? JSON.stringify(request.body)
+    : ''
+)
+
+app.get('/', (request, response) => {
+    response.send(
+        `<h1>
+            Go to the proper 
+            <a href="http://localhost:3001/api/persons">page</a>
+            or check 
+            <a href="http://localhost:3001/info">info</a>.
+        </h1>`)
+})
+
+
+// Get list of person API
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+// Information of the phonebook
+app.get('/info', (request, response) => {
+    // Get current date
+    const date = new Date().toString()
+    response.send(`<div>Phonebook has info for ${phonebook.length} people</div>
+    <div>${date}</div>`)
+})
+
+// Get information of the single phone entry in the phonebook
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => person.id === id)
+    if(person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+//Delete a phone entry from the phonebook
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebook = phonebook.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => (
+    Math.random() * 100000
+)
+
+//Create a new phone entry in the phonebook
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    console.log(person)
+    if(!person.name || !person.number) {
+        return response.status(400).json({
+            error: 'The name or number is missing'
+        })
+    }
+    else if (phonebook.find(entry => entry.name === person.name)) {
+        return response.status(400).json({
+            error: 'The name already exists in the phonebook'
+        })
+    }
+    let contact = {
+        id: generateId(),
+        name: person.name,
+        number: person.number
+    }
+    phonebook = phonebook.concat(contact)
+    response.json(contact)
+})
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
